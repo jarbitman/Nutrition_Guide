f3a2d41d73059ffaea26a84eed39a490ff4adda3
jewmanfoo <jewmanfoo@jewmanfoo.com>
2019-08-09 11:31:03 -0500

    added item pull

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
+$mysqli = new mysqli("10.80.0.3", "pbconnect", 'KS4DV42pYJ2eNSYB', "pbc2");
 include("header.php");
 $groups[1]="BREAKFAST / COFFEE";
-$groups[2]="SMOOTHIES";
+$groups[2]="SHAKES";
 $groups[3]="BOWLS/BAR-RITOS";
 $groups[4]="CHILIS/SOUPS";
 $groups[5]="SALADS/WRAPS";
 $groups[6]="KIDS MENU";
-
 echo " <div id=\"accordion\">";
 foreach ($groups as $key => $value) {
   echo "  <h3 style='background-color:#b2d235;color:#FFFFFF;'>$value</h3>
@@ -27,7 +27,36 @@ foreach ($groups as $key => $value) {
       <th class=\"table-sortable:numeric\"  style='background-color:#0e2244;color:#FFFFFF;'>SUGARS</th>
       <th class=\"table-sortable:numeric\"  style='background-color:#0e2244;color:#FFFFFF;'>PROTEIN</th>
       </tr>
+      </thead>
+      <tbody>
+      ";
+      $q="SELECT itemName,itemInfo FROM pbc_public_nutritional WHERE published=1 and itemSection='".$key."'";
+      $stmt = $mysqli->stmt_init();
+      $stmt->prepare($q);
+      $stmt->execute();
+      $result = $stmt->get_result();
+      while($row=$result->fetch_object()){
+        $info=json_decode($row->itemInfo);
+        echo "
+          <tr>
+          <td>".stripslashes($row->itemName)."</td>
+          <td>".stripslashes($info->Cal)."</td>
+          <td>".stripslashes($info->TF)."</td>
+          <td>".stripslashes($info->SF)."</td>
+          <td>".stripslashes($info->TRF)."</td>
+          <td>".stripslashes($info->COL)."</td>
+          <td>".stripslashes($info->SOD)."</td>
+          <td>".stripslashes($info->NC)."</td>
+          <td>".stripslashes($info->TC)."</td>
+          <td>".stripslashes($info->DF)."</td>
+          <td>".stripslashes($info->SG)."</td>
+          <td>".stripslashes($info->PR)."</td>
+          </tr>
+        ";
+      }
 
+echo   "
+        </tbody>
       </table>
   </div>";
 }
