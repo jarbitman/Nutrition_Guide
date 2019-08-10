dc4b215518ad317b22451c524017ebf79eb4488b
Jon Arbitman <jon@theproteinbar.com>
2019-08-10 12:03:48 -0500

    finished interpolated variables

--- modal.js
+++ modal.js
@@ -2,14 +2,16 @@ $(".itemName").on("click", function(e) {
   itemOptions = $("#" + e.target.id).data("options");
   var nutritionLabel = $("#modalNutritionLabelParent").html();
 
-  itemOptions.calories = itemOptions.Cal;
-  itemOptions.totalfat = itemOptions.TF;
+  itemOptions.totalfatperc = "" + Math.round(parseInt(itemOptions.TF.replace(/[^0-9]/g), '')/65 * 100);
+  itemOptions.satfatperc = "" + Math.round(parseInt(itemOptions.SF.replace(/[^0-9]/g), '')/20 * 100);
+  itemOptions.cholesterolperc = "" + Math.round(parseInt(itemOptions.CHO.replace(/[^0-9]/g), '')/300 * 100);
+  itemOptions.sodiumperc = "" + Math.round(parseInt(itemOptions.CHO.replace(/[^0-9]/g), '')/2400 * 100);
+  itemOptions.carbsperc = "" + Math.round(parseInt(itemOptions.NC.replace(/[^0-9]/g), '')/300 * 100);
+  itemOptions.fiberperc = "" + Math.round(parseInt(itemOptions.DF.replace(/[^0-9]/g), '')/25 * 100);
 
   nutritionLabel = nutritionLabel.replace(/\{\{([a-z]+)\}\}/gi, function(_, field) {
-    console.log(itemOptions);
-    console.log(`_: ${_}, field: ${field}, value ${itemOptions[field]}`);
     if (itemOptions[field]) {
-      return itemOptions[field];
+      return itemOptions[field].replace(/[^0-9]/g, '');
     } else {
       return '';
     }
