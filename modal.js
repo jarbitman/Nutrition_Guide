b5b11172604b656d25242a6c35f6e914332920ec
Jon Arbitman <jon@theproteinbar.com>
2019-08-10 11:38:27 -0500

    added value translations

--- modal.js
+++ modal.js
@@ -4,6 +4,9 @@ $(".itemName").on("click", function(e) {
   itemOptions = $("#" + e.target.id).data("options");
   var nutritionLabel = $("#modalNutritionLabelParent").html();
 
+  itemOptions.calories = itemOptions.Cal;
+  itemOptions.totalfat = itemOptions.TF;
+
   nutritionLabel = nutritionLabel.replace(/\{\{[a-z]+\}\}/gi, function(_, field) {
     console.log(`field: ${field}, value ${itemOptions[field]}`);
     if (itemOptions[field]) {
