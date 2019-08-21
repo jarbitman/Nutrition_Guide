f0dbd828066afff47bf62f805d82f249afb7c3f4
David Arbitman <david@arbitman.net>
2019-08-20 19:49:11 -0500

    HTML beautification

--- header.php
+++ header.php
@@ -1,91 +1,91 @@
 <!doctype html>
 <html class="no-js" lang="en-US">
-    <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title>Protein Bar & Kitchen</title>
-		<meta name="keywords" content="order online, colorado, denver, dc, chicago, healthy, to go, gluten free, vegan, bowls, breakfast, lunch, dinner, rewards">
-		<meta name="description" content="Welcome to Protein Bar. We are conveniently located in Chicago, Washington, D.C, Denver and Boulder. Our service, our stores and our work is focused on healthy food that can be served in a quick, engaging way for active, ﻿﻿﻿﻿on-the-go ﻿﻿﻿customers.">
-		<link rel="icon" href="https://media-cdn.getbento.com/dHMR3lgfR7xOrPIgJlMQ_pbkfav.png">
-	<link rel="canonical" href="https://www.theproteinbar.com/">
-	<meta name="Revisit-After" content="5 Days">
-	<meta name="Distribution" content="Global">
-	<meta name="Rating" content="General">
-		<meta property="og:site_name" content="Protein Bar & Kitchen">
-		<meta property="og:title" content="Protein Bar & Kitchen">
-		<meta property="og:type" content="website">
-		<meta property="og:url" content="https://www.theproteinbar.com/">
-			<meta property="og:description" content="Welcome to Protein Bar. We are conveniently located in Chicago, Washington, D.C, Denver and Boulder. Our service, our stores and our work is focused on healthy food that can be served in a quick, engaging way for active, ﻿﻿﻿﻿on-the-go ﻿﻿﻿customers.">
-			<meta property="og:image" content="https://images.getbento.com/lLXakZdwSQ6uVE7qz82h_pbklogo1.png?w=1200&fit=fill&auto=compress,format&h=600&bg=EDEDF1&pad=100">
-		<meta name="twitter:card" content="summary">
-		<meta name="twitter:title" content="Protein Bar & Kitchen">
-			<meta name="twitter:description" content="Welcome to Protein Bar. We are conveniently located in Chicago, Washington, D.C, Denver and Boulder. Our service, our stores and our work is focused on healthy food that can be served in a quick, engaging way for active, ﻿﻿﻿﻿on-the-go ﻿﻿﻿customers.">
-			<meta name="twitter:site" content="@theproteinbar">
-			<meta name="twitter:creator" content="@theproteinbar">
-			<meta name="twitter:image" content="https://images.getbento.com/lLXakZdwSQ6uVE7qz82h_pbklogo1.png?w=1200&fit=fill&auto=compress,format&h=600&bg=EDEDF1&pad=100">
-	<script type="text/javascript" src="https://theme-assets.getbento.com/sensei/a82ed78.sensei/assets/js/head.min.js"></script>
-	<link rel="stylesheet" href="//assets-cdn-refresh.getbento.com/stylesheet/theproteinbar/scss/main.6a1635f8cad77f1901297b3217c1b36b.scss" />
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="//c2.theproteinbar.com/Nutrition_Guide/label.css">
-  <style>
-    th {
-      color: #FFFFFF;
-      font-family: 'Trade Gothic Bold Condensed';
-    }
-    tr.alternate{
-      background-color: #ccf;
-    }
-    td {
-      font-family: "Lora";
-    }
-    .itemName:hover {
-      text-decoration: underline;
-    }
-    .indent-value {
-      text-indent: 15px;
-    }
-    .ui-widget-header{
-      font-family: "Trade Gothic Bold Condensed";
-      font-size: 1.75em;
-      text-transform: uppercase;
-      overflow-wrap: break-word;
-      background-color: #F36C21;
-      color: #FFFFFF;
-    }
-    .ui-dialog .ui-dialog-title {
-      white-space: wrap;
-      overflow: wrap;
-    }
-    .nutrition-item-label {
-      font-family: "Trade Gothic Bold Condensed";
-      color:#0E2244;
-      font-size:1.5em;
-      text-transform:uppercase;
-      letter-spacing:2px;
-    }
-    .nutrition-item {
-      font-family: "Lora";
-      font-size: 1rem;
-      line-height:1.5;
-      color:#444;
-    }
-    @media all and (max-width: 767px) {
-      td.mobileShowHide{
-        display:none;
-        width: 0;
-        height: 0;
-        opacity: 0;
-        visibility: collapse;
+  <head>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  	<meta http-equiv="x-ua-compatible" content="ie=edge">
+  	<title>Protein Bar & Kitchen</title>
+  	<meta name="keywords" content="order online, colorado, denver, dc, chicago, healthy, to go, gluten free, vegan, bowls, breakfast, lunch, dinner, rewards">
+  	<meta name="description" content="Welcome to Protein Bar. We are conveniently located in Chicago, Washington, D.C, Denver and Boulder. Our service, our stores and our work is focused on healthy food that can be served in a quick, engaging way for active, ﻿﻿﻿﻿on-the-go ﻿﻿﻿customers.">
+  	<link rel="icon" href="https://media-cdn.getbento.com/dHMR3lgfR7xOrPIgJlMQ_pbkfav.png">
+  	<link rel="canonical" href="https://www.theproteinbar.com/">
+  	<meta name="Revisit-After" content="5 Days">
+  	<meta name="Distribution" content="Global">
+  	<meta name="Rating" content="General">
+  	<meta property="og:site_name" content="Protein Bar & Kitchen">
+  	<meta property="og:title" content="Protein Bar & Kitchen">
+  	<meta property="og:type" content="website">
+  	<meta property="og:url" content="https://www.theproteinbar.com/">
+  	<meta property="og:description" content="Welcome to Protein Bar. We are conveniently located in Chicago, Washington, D.C, Denver and Boulder. Our service, our stores and our work is focused on healthy food that can be served in a quick, engaging way for active, ﻿﻿﻿﻿on-the-go ﻿﻿﻿customers.">
+  	<meta property="og:image" content="https://images.getbento.com/lLXakZdwSQ6uVE7qz82h_pbklogo1.png?w=1200&amp;fit=fill&amp;auto=compress,format&amp;h=600&amp;bg=EDEDF1&amp;pad=100">
+  	<meta name="twitter:card" content="summary">
+  	<meta name="twitter:title" content="Protein Bar & Kitchen">
+  	<meta name="twitter:description" content="Welcome to Protein Bar. We are conveniently located in Chicago, Washington, D.C, Denver and Boulder. Our service, our stores and our work is focused on healthy food that can be served in a quick, engaging way for active, ﻿﻿﻿﻿on-the-go ﻿﻿﻿customers.">
+  	<meta name="twitter:site" content="@theproteinbar">
+  	<meta name="twitter:creator" content="@theproteinbar">
+  	<meta name="twitter:image" content="https://images.getbento.com/lLXakZdwSQ6uVE7qz82h_pbklogo1.png?w=1200&amp;fit=fill&amp;auto=compress,format&amp;h=600&amp;bg=EDEDF1&amp;pad=100">
+  	<script type="text/javascript" src="https://theme-assets.getbento.com/sensei/a82ed78.sensei/assets/js/head.min.js"></script>
+  	<link rel="stylesheet" href="//assets-cdn-refresh.getbento.com/stylesheet/theproteinbar/scss/main.6a1635f8cad77f1901297b3217c1b36b.scss" />
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="//c2.theproteinbar.com/Nutrition_Guide/label.css">
+    <style>
+      th {
+        color: #ffffff;
+        font-family: 'Trade Gothic Bold Condensed';
       }
-      th.mobileShowHide{
-        display: none;
-        width: 0;
-        height: 0;
-        opacity: 0;
-        visibility: collapse;
+      tr.alternate {
+        background-color: #ccf;
       }
-    }
-  </style>
-</head>
-<body>
+      td {
+        font-family: "Lora";
+      }
+      .itemName:hover {
+        text-decoration: underline;
+      }
+      .indent-value {
+        text-indent: 15px;
+      }
+      .ui-widget-header {
+        font-family: "Trade Gothic Bold Condensed";
+        font-size: 1.75em;
+        text-transform: uppercase;
+        overflow-wrap: break-word;
+        background-color: #f36c21;
+        color: #ffffff;
+      }
+      .ui-dialog .ui-dialog-title {
+        white-space: wrap;
+        overflow: wrap;
+      }
+      .nutrition-item-label {
+        font-family: "Trade Gothic Bold Condensed";
+        color: #0E2244;
+        font-size: 1.5em;
+        text-transform: uppercase;
+        letter-spacing: 2px;
+      }
+      .nutrition-item {
+        font-family: "Lora";
+        font-size: 1rem;
+        line-height: 1.5;
+        color: #444;
+      }
+      @media all and (max-width: 767px) {
+        td.mobileShowHide{
+          display: none;
+          width: 0;
+          height: 0;
+          opacity: 0;
+          visibility: collapse;
+        }
+        th.mobileShowHide {
+          display: none;
+          width: 0;
+          height: 0;
+          opacity: 0;
+          visibility: collapse;
+        }
+      }
+    </style>
+  </head>
+  <body>
