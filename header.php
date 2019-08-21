3a565646de8abd0fbfd4a6648ef036ea1f0537e1
jewmanfoo <jewmanfoo@jewmanfoo.com>
2019-08-21 11:42:40 -0500

    adjusted table sort

--- header.php
+++ header.php
@@ -29,8 +29,8 @@
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="//c2.theproteinbar.com/Nutrition_Guide/label.css">
     <style>
-      table.sort13 th.table-sorted-asc { background-image:url("icons/ascending.gif"); }
-      table.sort13 th.table-sorted-desc { background-image:url("icons/descending.gif"); }
+      th.table-sorted-asc { background-image:url("icons/ascending.gif"); }
+      th.table-sorted-desc { background-image:url("icons/descending.gif"); }
       .iconset {
 	       margin:5px;
 	       border:1px solid #cccccc;
