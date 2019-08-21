60187dee0c5c4a88765398533879318612ed2cda
jewmanfoo <jewmanfoo@jewmanfoo.com>
2019-08-21 11:33:33 -0500

    changed icon names

--- header.php
+++ header.php
@@ -30,8 +30,8 @@
     <link rel="stylesheet" href="//c2.theproteinbar.com/Nutrition_Guide/label.css">
     <style>
       table.sort13 th.table-sortable { background-image:none; }
-      table.sort13 th.table-sorted-asc { background-image:url("icons/13_ascending.gif"); }
-      table.sort13 th.table-sorted-desc { background-image:url("icons/13_descending.gif"); }
+      table.sort13 th.table-sorted-asc { background-image:url("icons/ascending.gif"); }
+      table.sort13 th.table-sorted-desc { background-image:url("icons/descending.gif"); }
       .iconset {
 	       margin:5px;
 	       border:1px solid #cccccc;
