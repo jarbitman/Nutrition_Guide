1b1c3448d61d0d3575a47bae02eddf960fa9d0e7
jewmanfoo <jewmanfoo@jewmanfoo.com>
2019-11-20 15:21:49 -0600

    updated fonts

--- pdfBuild.php
+++ pdfBuild.php
@@ -1,15 +1,11 @@
 <?php
 $stylesheet="
-@font-face{font-family:\"Gotham Black\";src:url(\"./font/Gotham-Black.ttf\") format(\"truetype\")}
-@font-face{font-family:\"Lora\";src:url(\"./font/Lora-Regular.ttf\") format(\"truetype\");font-weight:700}
-@font-face{font-family:\"Trade Gothic Bold Condensed Oblique\";src:url(\"./font/TradeGothicBoldCondensedNo20Oblique.ttf\") format(\"truetype\")}
-@font-face{font-family:\"Trade Gothic Bold Condensed\";src:url(\"./font/TradeGothicBoldCondensedNo20.ttf\") format(\"truetype\")}
 .bluehead{
-  font-family:\"Lora\";
+  font-family:\"lora\";
   color:#FFFFFF;
 }
 .greenhead{
-  font-family:\"Gotham Black\";
+  font-family:\"gothamblack\";
   color:#FFFFFF;
 }
 ";
@@ -19,7 +15,7 @@ $pdfHead="
 $pdfFoot="</body>";
 require_once __DIR__ . '/vendor/autoload.php';
 
-$html = '<div style="font-family:Trade Gothic Bold Condensed;color:#FFFFFF;font-weight:400;text-transform:uppercase;letter-spacing:2px;font-size:30;">NUTRITIONAL INFORMATION</div>
+$html = '<div style="font-family:tradegothic;color:#FFFFFF;font-weight:400;text-transform:uppercase;letter-spacing:2px;font-size:30;">NUTRITIONAL INFORMATION</div>
 ';
 foreach ($groups as $key => $value) {
   $html.= "
@@ -63,6 +59,10 @@ foreach ($groups as $key => $value) {
     $html.="
     </table></div>";
 }
+$defaultConfig = (new Mpdf\Config\ConfigVariables())->getDefaults();
+$fontDirs = $defaultConfig['fontDir'];
+$defaultFontConfig = (new Mpdf\Config\FontVariables())->getDefaults();
+$fontData = $defaultFontConfig['fontdata'];
 $mpdf = new \Mpdf\Mpdf([
 	'mode' => 'c',
   'format' => 'A4-P',
@@ -71,8 +71,25 @@ $mpdf = new \Mpdf\Mpdf([
 	'margin_top' => 0,
 	'margin_bottom' => 5,
 	'margin_header' => 0,
-	'margin_footer' => 0
+	'margin_footer' => 0,
+  'fontDir' => array_merge($fontDirs, [__DIR__]),
+'fontdata' => $fontData + [
+  'lora' => [
+    'R' => 'font/lora-regular.ttf',
+    'I' => 'font/lora-italic.ttf',
+  ],
+  'gothamblack' => [
+    'R' => 'font/gotham-black.ttf',
+    'I' => 'font/gotham-black.ttf',
+  ],
+  'tradegothic' => [
+    'R' => 'font/tradegothic.ttf',
+    'I' => 'font/tradegothici.ttf',
+  ]
+],
+'default_font' => 'lora'
 ]);
+$mpdf->AddFontDirectory("./font");
 $mpdf->WriteHTML($stylesheet,\Mpdf\HTMLParserMode::HEADER_CSS);
 $mpdf->WriteHTML($pdfHead.$html.$pdfFoot);
 $mpdf->Output();
